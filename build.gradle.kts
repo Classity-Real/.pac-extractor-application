de6d942a1b69e4e421de22fd0382f13018642fc0
Mountain <uiportsarchive@gmail.com>
2026-08-11 05:31:49 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    // Required since Kotlin 2.0: Compose no longer gets its compiler
+    // bundled into the Kotlin plugin, it needs this plugin applied
+    // wherever buildFeatures.compose = true is set (see app/build.gradle.kts).
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
 }
